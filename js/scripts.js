bfca2158ebf34e372f28392d4a1d47b1a6e89c6c
rjruiz-dev <rjruizsf@gmail.com>
2021-06-24 15:04:36 -0300

    media query para distintas resoluciones de pantallas

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+//Menu Hamburguesa
+document.querySelector('#menu').addEventListener(
+    'click', () => {
+        document.querySelector('.menu').classList.toggle('openMenu');
+        if(document.querySelector('.menu').classList.contains('openMenu')){
+            document.querySelector('#menu').innerHTML = '&times;'
+        } else {
+            document.querySelector('#menu').innerHTML = '&equiv;'
+        }
+    }
+)
+
+// Galeria slide
+let galeria = document.querySelectorAll('.galeria li');
+
+// boton anterior
+document.querySelector('#prev').addEventListener(
+    'click', () => {
+        for(i = 0; i < galeria.length; i++){
+            if (galeria[i].classList.contains('active')){
+               galeria[i].classList.remove('active'); 
+               if( i > 0 ){
+                   nextItem = galeria[i - 1];
+               } else {
+                   nextItem = galeria[galeria.length -1];
+               }             
+            }
+        }
+        nextItem.classList.add('active');
+    }
+)
+
+// boton siguiente
+document.querySelector('#next').addEventListener(
+    'click', () => {
+        for(i = 0; i < galeria.length; i++){
+            if (galeria[i].classList.contains('active')){
+               galeria[i].classList.remove('active'); 
+               if( i < galeria.length - 1 ){
+                   nextItem = galeria[i + 1];
+               } else {
+                   nextItem = galeria[0];
+               }             
+            }
+        }
+        nextItem.classList.add('active');
+    }
+)
